5a0aa956a8cff22ac5b8eb7a7973c8850f32112e
potato-panda <76853151+potato-panda@users.noreply.github.com>
2024-09-08 10:19:41 -0400

    Rename Medusa Store

--- luvhavencove-store-storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ luvhavencove-store-storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -56,10 +56,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 
   return {
-    title: `${product.title} | Medusa Store`,
+    title: `${product.title} | LuvHavenCove`,
     description: `${product.title}`,
     openGraph: {
-      title: `${product.title} | Medusa Store`,
+      title: `${product.title} | LuvHavenCove`,
       description: `${product.title}`,
       images: product.thumbnail ? [product.thumbnail] : [],
     },
