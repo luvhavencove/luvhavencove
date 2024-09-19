92d4769b35d58bf56e3f60bdf07adc196144111a
potato-panda <76853151+potato-panda@users.noreply.github.com>
2024-09-19 20:03:40 +0000

    Enable Redis plugins, and updated and cleaned up configurations
    Enable Redis plugins, and updated and cleaned up configurations

--- luvhavencove-store/medusa-config.js
+++ luvhavencove-store/medusa-config.js
@@ -59,18 +59,20 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
-      redisUrl: REDIS_URL
+      redisUrl: REDIS_URL,
+      queueName: "events-queue",
     }
   },
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
-      redisUrl: REDIS_URL
+      namespace: "medusa",
+      redisUrl: REDIS_URL,
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -81,7 +83,7 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+  redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
