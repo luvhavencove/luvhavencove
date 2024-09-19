aef9186466b90d40a72c48804980761bc1f19217
potato-panda <76853151+potato-panda@users.noreply.github.com>
2024-09-19 20:28:40 +0000

    Added Meilisearch plugin

--- luvhavencove-store/run.sh
+++ luvhavencove-store/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Load environment variables
+MEILISEARCH_API_KEY=$(cat $MEILISEARCH_API_KEY_FILE)
+export MEILISEARCH_API_KEY
+
 # Remove existing Meilisearch Public API key
 if [[ -f /data/public/meilisearch_public_key ]]; then
   rm /data/public/meilisearch_public_key
@@ -7,13 +11,13 @@ fi
 
 # Obtain Meilisearch Public API key
 curl -X GET "$MEILISEARCH_HOST/keys" \
-  -H "Authorization: Bearer $(cat "$MEILISEARCH_API_KEY")" \
+  -H "Authorization: Bearer $MEILISEARCH_API_KEY" \
   -s |
   jq ".results[0].key" |
-  sed 's/"//g' >/data/public/meilisearch_public_key
+  sed 's/"//g' > /data/public/meilisearch_public_key
 
 # Run Medusa Seeding
-# npx medusa seed -f ./data/seed.json
+npx medusa seed -f ./data/seed.json || true
 
 if [[ "$1" == "production" ]]; then
   echo "Running Backend in 'production'."
