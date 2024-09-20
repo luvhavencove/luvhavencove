03afcaf685d87c119a0b6722f50e33e0d0d6d7f0
potato-panda <76853151+potato-panda@users.noreply.github.com>
2024-09-20 19:41:26 +0000

    Add backend Paypal processor

--- luvhavencove-store/medusa-config.js
+++ luvhavencove-store/medusa-config.js
@@ -86,7 +86,16 @@ const plugins = [
         },
       },
     },
-  }
+  },
+  {
+    resolve: `medusa-payment-paypal`,
+    options: {
+      sandbox: process.env.PAYPAL_SANDBOX,
+      clientId: process.env.PAYPAL_CLIENT_ID,
+      clientSecret: process.env.PAYPAL_CLIENT_SECRET,
+      authWebhookId: process.env.PAYPAL_AUTH_WEBHOOK_ID,
+    },
+  },
 ];
 
 const modules = {
