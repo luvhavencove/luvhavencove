36330b461070ff9e45cbb972ae300fe0e72ad456
potato-panda <76853151+potato-panda@users.noreply.github.com>
2024-09-24 20:10:32 +0000

    Clarify script affect files

--- scripts/set-envvars-secrets.sh
+++ scripts/set-envvars-secrets.sh
@@ -16,17 +16,23 @@ set -a
 if [[ -f "$project_root/.env" ]]; then
   source "$project_root/.env"
   templates=$(echo $(find $project_root/secrets -maxdepth 1 -type f -name "*.template") $(find $project_root/luvhavencove-store -maxdepth 1 -type f -name ".env.template") $(find $project_root/luvhavencove-store-storefront -maxdepth 1 -type f -name ".env.template"))
+  echo "Setting environment variables and secrets from .env"
   for template in ${templates}; do
-    envsubst < ${template} >$(echo ${template} | sed 's#.template##')
+    target=$(echo ${template} | sed 's#.template##')
+    envsubst < ${template} >target
+    echo "Set $target"
   done
-  echo "Set environment variables and secrets from .env"
+  echo "Finishing setting environment variables and secrets from .env"
 fi
 
 if [[ -f "$project_root/.env.production" ]]; then
   source "$project_root/.env.production"
   templates=$(echo $(find $project_root/secrets/production -maxdepth 1 -type f -name "*.template") $(find $project_root/luvhavencove-store -maxdepth 1 -type f -name ".env.production.template") $(find $project_root/luvhavencove-store-storefront -maxdepth 1 -type f -name ".env.production.template"))
+  echo "Setting production environment variables and secrets from .env.production"
   for template in ${templates}; do
-    envsubst < ${template} >$(echo ${template} | sed 's#.template##')
+    target=$(echo ${template} | sed 's#.template##')
+    envsubst < ${template} >$target
+    echo "Set $target"
   done
-  echo "Set production environment variables and secrets from .env.production"
+  echo "Finishing setting production environment variables and secrets from .env.production"
 fi
